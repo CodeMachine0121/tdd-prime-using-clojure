6cc5e3da10cf90c7d432bd0e416b4edc4aa1e8e4
james.hsueh <james.hsueh@titansoft.com.sg>
2025-01-30 04:24:10 +0800

    feat: add case for 5, 6, 7, 8

--- test/tdd_prime_number/core_test.clj
+++ test/tdd_prime_number/core_test.clj
@@ -18,4 +18,20 @@
   (testing "should got [2 2]"
       (is (= [2 2] (prime 4)))
   )
+
+  (testing "should got [5]"
+      (is (= [5] (prime 5)))
+  )
+
+  (testing "should got [2 3]"
+      (is (= [2 3] (prime 6)))
+  )
+
+  (testing "should got [7]"
+      (is (= [7] (prime 7)))
+  )
+
+  (testing "should got [2 2 2]"
+      (is (= [2 2 2] (prime 8)))
+  )
 )
